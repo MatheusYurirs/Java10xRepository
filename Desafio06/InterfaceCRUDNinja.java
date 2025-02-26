56d534a461f4dc05e07d7bd8e8f28808a307795a
Matheus Yuri Silva <80913079+MatheusYurirs@users.noreply.github.com>
2025-02-26 20:50:12 -0300

    Desafio06
    Implementação de Crud de Cadastro de Ninjas

---
+++ Desafio06/InterfaceCRUDNinja.java
@@ -0,0 +1,8 @@
+package NivelIntermediario.Desafio06;
+
+public interface InterfaceCRUDNinja<T> {
+    void adicionarOPrimeiro(Ninja object);
+    void removerOPrimeiro();
+    void listarTodos();
+    void procurar(int i);
+}
