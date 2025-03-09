49b216b3dab0ade4599f51e3ef2ecb7857e5a554
Matheus Yuri Silva <80913079+MatheusYurirs@users.noreply.github.com>
2025-03-09 10:35:08 -0300

    Delete Desafio06 directory

--- Desafio06/InterfaceCRUDNinja.java
+++
@@ -1,8 +0,0 @@
-package NivelIntermediario.Desafio06;
-
-public interface InterfaceCRUDNinja<T> {
-    void adicionarOPrimeiro(Ninja object);
-    void removerOPrimeiro();
-    void listarTodos();
-    void procurar(int i);
-}
